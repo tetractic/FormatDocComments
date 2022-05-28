6984260222941aec58f297434bb34ca329551b9f
Carl Reinke <carlreinke@users.noreply.github.com>
2022-05-28 09:33:47 -0600

    Relicense to LGPL v3

--- FormatDocXml/DocXmlFormattingOptions.cs
+++ FormatDocXml/DocXmlFormattingOptions.cs
@@ -1,20 +1,12 @@
-﻿//
-// Copyright (C) 2018  Carl Reinke
+﻿// Copyright 2018 Carl Reinke
 //
-// This file is part of FormatDocXML.
+// This file is part of a library that is licensed under the terms of GNU Lesser
+// General Public License version 3 as published by the Free Software
+// Foundation.
 //
-// This program is free software; you can redistribute it and/or modify it under the terms of the
-// GNU Lesser General Public License as published by the Free Software Foundation; either version 2
-// of the License, or (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
-// even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-// Lesser General Public License for more details.
-//
-// You should have received a copy of the GNU Lesser General Public License along with this program;
-// if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
-// 02110-1301, USA.
-//
+// This license does not grant rights under trademark law for use of any trade
+// names, trademarks, or service marks.
+
 using Microsoft.CodeAnalysis.Options;
 
 namespace FormatDocXml
--- FormatDocXml/Properties/AssemblyInfo.cs
+++ FormatDocXml/Properties/AssemblyInfo.cs
@@ -1,4 +1,13 @@
-﻿using System.Reflection;
+﻿// Copyright 2018 Carl Reinke
+//
+// This file is part of a library that is licensed under the terms of GNU Lesser
+// General Public License version 3 as published by the Free Software
+// Foundation.
+//
+// This license does not grant rights under trademark law for use of any trade
+// names, trademarks, or service marks.
+
+using System.Reflection;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
